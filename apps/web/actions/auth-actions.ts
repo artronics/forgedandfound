3a6c58f3ff36cfca04d4d904bae614d62067d0c9
Jalal Hosseini <jhosseini.dev@gmail.com>
2026-07-16 15:56:48 +0100

    fix usermenu multiple click on logout

--- apps/web/actions/auth-actions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import {signOut} from "@/auth";
-import {refresh} from "next/cache";
-
-export async function signOutWithCognito(returnTo?: string) {
-  await signOut({redirectTo: returnTo});
-  refresh();
-}
